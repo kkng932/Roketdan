178f2bb40db3f04e884afcca27ccd06c04ad36bd
kkng932 <kkng93279@naver.com>
2025-05-28 19:56:25 +0900

    feat: 배경 스크롤러

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField]
+    GameObject groundObj;
+    [SerializeField]
+    GameObject backgroundObj;
+    [SerializeField]
+    float groundSpeed;
+    [SerializeField]
+    float backgroundSpeed;
+
+    Vector2 groundStartPos;
+    Vector2 backgroundStartPos;
+
+    float groundWidth;
+    float backgroundWidth;
+    private void Start()
+    {
+        groundStartPos = groundObj.transform.position;
+        backgroundStartPos = backgroundObj.transform.position;
+
+        groundWidth = groundObj.GetComponent<SpriteRenderer>().bounds.size.x;
+        backgroundWidth = backgroundObj.GetComponent<SpriteRenderer>().bounds.size.x;
+        
+    }
+    private void Update()
+    {
+        groundObj.transform.Translate(Vector2.left * groundSpeed * Time.deltaTime);
+        backgroundObj.transform.Translate(Vector2.left * backgroundSpeed * Time.deltaTime);
+
+        if(groundObj.transform.position.x <= groundStartPos.x - groundWidth)
+        {
+            groundObj.transform.position = groundStartPos;
+        }
+        if(backgroundObj.transform.position.x <= backgroundStartPos.x - backgroundWidth)
+        {
+            backgroundObj.transform.position = backgroundStartPos;
+        }
+    }
+}
