daa01c8e25990c76459d71a7a100822d97dbceed
kkng932 <kkng93279@naver.com>
2025-05-28 19:59:52 +0900

    feat: 싱글톤

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,39 @@
+// 유니티로 배우는 게임 디자인 패턴 : 싱글톤
+
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    instance = obj.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
