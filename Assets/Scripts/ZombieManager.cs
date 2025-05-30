9ba32d5f62a819fbb2036d1906f04fc4a05f034f
kkng932 <kkng93279@naver.com>
2025-05-30 19:14:08 +0900

    refactor: FSM 형식으로 리팩토링

--- Assets/Scripts/ZombieManager.cs
+++ Assets/Scripts/ZombieManager.cs
@@ -7,123 +7,65 @@ using UnityEngine;
 
 public class ZombieManager : MonoBehaviour
 {
+    ZombieState currState;
     // 이동 속도
-    [SerializeField]
-    float speed;
+    public float speed;
     
     // 맨 뒤 몬스터가 올라가는 힘
-    [SerializeField]
-    float climbForce;
+    public float climbForce;
 
-    Rigidbody2D rb;
-    Animator animator;
+    public Rigidbody2D rb;
+    public Animator animator;
     CapsuleCollider2D col;
 
-    // 박스 닿아있는지 여부 
-    bool isTouchingBox = false;
-    // 줄 중간에 있는지 여부
-    bool isMiddle = false;
+    // 현재 밀려나고 있는 중
+    bool isPushing = false;
     // 현재 닿은 위치 
     Vector3 contactPoint;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         col = GetComponent<CapsuleCollider2D>();
+        ChangeState(new ZombieWalkState(this));
+        
     }
-    
-    void FixedUpdate()
+    public void ChangeState(ZombieState newState)
     {
-        rb.velocity = new Vector2(-speed, rb.velocity.y);
+        
+        currState?.Exit();
+        currState = newState;
+        currState.Enter();
+    }
+    private void Update()
+    {
+        currState?.Update();
+    }
+    private void FixedUpdate()
+    {
+        currState?.FixedUpdate();
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Box"))
-        {
-            isTouchingBox = true;
-            Attack();
-        }
-        if(collision.gameObject.CompareTag("Monster"))
-        {
-            Vector2 normal = collision.contacts[0].normal;
-            if (isTouchingBox)
-            {
-                contactPoint = collision.contacts[0].point;
-                float topPos = transform.position.y + col.offset.y + col.size.y / 2f;
-                // 머리 위에 몬스터가 타면 밀려남
-                if (topPos <= contactPoint.y + 0.05f)
-                {
-                    Push();
-                }
-            }
-            else
-            {
-                // 오른쪽에 몬스터가 닿아있으면 중간에 있다고 판단
-                if (Vector2.Dot(normal, Vector2.left) > 0.5f)
-                    isMiddle = true;
-            }
-        }
-
+        currState.OnCollisionEnter2D(collision);
+        
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Box"))
-            isTouchingBox = true;
-        if(collision.gameObject.CompareTag("Monster"))
-        {
-            Vector2 normal = collision.contacts[0].normal;
-            // 오른쪽에 몬스터가 닿아있으면 중간에 있다고 판단
-            if (Vector2.Dot(normal, Vector2.left) > 0.5f)
-                isMiddle = true;
-            if(!isTouchingBox && !isMiddle)
-            {
-                if (normal.y >= 0f && normal.y < 0.8f)
-                    rb.velocity = new Vector2(-speed, climbForce * (1 - normal.y));
-                else
-                    rb.velocity = new Vector2(-speed, 0f);
-            }
-        }
+        currState.OnCollisionStay2D(collision);
+      
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Box"))
-            isTouchingBox = false;
-        if (collision.gameObject.CompareTag("Monster"))
-            isMiddle = false;
-
+        currState.OnCollisionExit2D(collision);
     }
-    void Attack()
-    {
-        StartCoroutine(IAttack());
-    }
+    
     public void OnAttack()
     {
         
     }
-    IEnumerator IAttack()
-    {
-        while(isTouchingBox)
-        {
-            animator.Play("Attack");
-            yield return new WaitForSeconds(1f);
-        }
-    }
-    // 밀려남 
-    void Push()
-    {
-        StartCoroutine(IPush());
-    }
-    IEnumerator IPush()
-    {
-        float time = 0f;
-        float duration = 2f;
-        while(time < 2f)
-        {
-            time += Time.deltaTime;
-            transform.position = Vector2.Lerp(transform.position, new Vector2(0f,transform.position.y), time / duration);
-            yield return null;
-        }
-    }
+    
     // 위치 확인 디버깅 기즈모
     private void OnDrawGizmos()
     {
@@ -134,5 +76,158 @@ public class ZombieManager : MonoBehaviour
         // 노란 원: 충돌 위치 확인
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(contactPoint, 0.1f);
+    }
+}
+public abstract class ZombieState
+{
+    protected ZombieManager zombieMgr;
+    public ZombieState(ZombieManager zombieMgr)
+    {
+        this.zombieMgr = zombieMgr;
+    }
+    public virtual void Enter() { }
+    public virtual void Exit() { }
+    public virtual void Update() { }
+    public virtual void FixedUpdate() { }
+    public virtual void OnCollisionEnter2D(Collision2D collision) { }
+    public virtual void OnCollisionStay2D(Collision2D collision) { }
+    public virtual void OnCollisionExit2D(Collision2D collision) { }
+}
+
+public class ZombieWalkState : ZombieState
+{
+    public ZombieWalkState(ZombieManager zombieMgr) : base(zombieMgr) { }
+
+    public override void FixedUpdate()
+    {
+        zombieMgr.rb.velocity = new Vector2(-zombieMgr.speed, zombieMgr.rb.velocity.y);
+    }
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Box"))
+        {
+            zombieMgr.ChangeState(new ZombieAttackState(zombieMgr));
+        }
+        if(collision.gameObject.CompareTag("Monster"))
+        {
+            // 왼쪽에 접촉했을 때 
+            Vector2 normal = collision.contacts[0].normal;
+            if (Vector2.Dot(normal, Vector2.right) > 0.5f)
+                zombieMgr.ChangeState(new ZombieStopState(zombieMgr));
+        }
+    }
+}
+public class ZombieStopState : ZombieState
+{
+    // 현재 줄 마지막인지
+    bool isLast = false;
+    // 마지막에 있고 지난 시간
+    float lastTime = 0f;
+    public ZombieStopState(ZombieManager zombieMgr) : base(zombieMgr) { }
+    public override void Enter()
+    {
+        lastTime = 0f;
+    }
+    public override void FixedUpdate()
+    {
+        zombieMgr.rb.velocity = new Vector2(0f, zombieMgr.rb.velocity.y);
+    }
+    public override void Update()
+    {
+        if (isLast)
+        {
+            lastTime += Time.deltaTime;
+            if (lastTime > 0.5f)
+            {
+                zombieMgr.ChangeState(new ZombieClimbState(zombieMgr));
+            }
+        }
+    }
+    public override void Exit()
+    {
+        lastTime = 0f;
+    }
+    public override void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Monster"))
+        {
+            Vector2 normal = collision.contacts[0].normal;
+            // 뒤에 몬스터가 닿을 때
+            if (Vector2.Dot(normal, Vector2.left) > 0.5f)
+                isLast = false;
+        }
+    }
+    public override void OnCollisionExit2D(Collision2D collision)
+    {
+        zombieMgr.ChangeState(new ZombieWalkState(zombieMgr));
+    }
+
+}
+public class ZombieClimbState : ZombieState
+{
+    public ZombieClimbState(ZombieManager zombieMgr) : base(zombieMgr) { }
+    public override void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Box"))
+            zombieMgr.ChangeState(new ZombieAttackState(zombieMgr));
+        if (collision.gameObject.CompareTag("Monster"))
+        {
+            Vector2 normal = collision.contacts[0].normal;
+            zombieMgr.rb.velocity = new Vector2(-zombieMgr.speed, zombieMgr.climbForce * (1 - normal.y));
+        }
+        
+        
+    }
+}
+public class ZombieAttackState : ZombieState
+{
+    bool isPushing = false;
+    Coroutine attackCrt;
+    public ZombieAttackState(ZombieManager zombieMgr) : base(zombieMgr) { }
+    public override void Enter()
+    {
+        attackCrt = zombieMgr.StartCoroutine(IEAttack());
+    }
+    public override void Exit()
+    {
+        if(attackCrt != null)
+            zombieMgr.StopCoroutine(attackCrt);
+    }
+    IEnumerator IEAttack()
+    {
+        while(true)
+        {
+            zombieMgr.animator.Play("Attack");
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    public override void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Monster"))
+        {
+            Vector2 normal = collision.contacts[0].normal;
+            if (normal.y <= -0.95f)
+                Push();
+        }
+    }
+    // 밀려남 
+    void Push()
+    {
+        if (isPushing) return;
+        zombieMgr.StartCoroutine(IPush());
+    }
+    IEnumerator IPush()
+    {
+        isPushing = true;
+        float time = 0f;
+        float duration = 1f;
+        while (time < 1f)
+        {
+            time += Time.deltaTime;
+            zombieMgr.transform.position = Vector2.Lerp(zombieMgr.transform.position, new Vector2(0f, zombieMgr.transform.position.y), time / duration);
+            yield return null;
+        }
+        isPushing = false;
+        zombieMgr.ChangeState(new ZombieWalkState(zombieMgr));
     }
 }
