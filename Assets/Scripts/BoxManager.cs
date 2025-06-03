0bbf22d75902798fa46673f8914f7562a3a294f4
kkng932 <kkng93279@naver.com>
2025-06-03 17:18:09 +0900

    feat: 박스 데미지 받았을 때 반짝이는 효과 추가

--- Assets/Scripts/BoxManager.cs
+++ Assets/Scripts/BoxManager.cs
@@ -7,9 +7,22 @@ public class BoxManager : MonoBehaviour
 {
     [SerializeField]
     Slider hpSlider;
+    [SerializeField]
+    private SpriteRenderer boxSr;
+
     float MAX_HP = 100f;
     float currHp;
 
+    private Material flashMat;
+    // 반짝이는 시간
+    float flashTime = 1f;
+    Coroutine flashClr;
+    
+
+    private void Awake()
+    {
+        flashMat = boxSr.material;
+    }
     private void OnEnable()
     {
         SetHP(MAX_HP);
@@ -21,10 +34,33 @@ public class BoxManager : MonoBehaviour
         hpSlider.value = currHp / MAX_HP;
     }
     // 데미지 받음
+    [ContextMenu("TakeDamage")]
+    public void Debug_TakeDamage()
+    {
+        TakeDamage(10f);
+    }
     public void TakeDamage(float damage)
     {
         currHp -= damage;
+        Flash();
         SetHP(currHp);
     }
-
+    private void Flash()
+    {
+        if (flashClr != null) StopCoroutine(flashClr);
+        flashClr = StartCoroutine(IEFlash());
+    }
+    IEnumerator IEFlash()
+    {
+        float t = 0f;
+        while (t < flashTime)
+        {
+            float amount = Mathf.Lerp(1f, 0f, t / flashTime);
+            flashMat.SetFloat("flashAmount", amount);
+            t += Time.deltaTime;
+            yield return null;
+            
+        }
+        flashMat.SetFloat("flashAmount", 0f);
+    }
 }
